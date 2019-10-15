99df8aa9466952f1659aef4015e1c52e0176c779
Sara Gradinarska <saragradinarska@Saras-MacBook-Air-8.local>
2019-10-15 22:46:48 +0800

    change to component

--- src/App.js
+++ src/App.js
@@ -6,73 +6,34 @@ import './App.css';
 function App() {
     return (
       <div>
-
-
-        <div className="container">
-          <div className="article-date">
-            <p>Jan. 30, 2018</p>
-          </div>
-          <div className="article-content">
-
-            <p >THE SHIFT</p>
-            <h1>Kodak's Dubious Blockchain Gamble</h1>
-            <div>
-              What's a 130-year-old company doing dabbling in cryptocurrency? Either revolutionizing digital rights management or trying to make a quick buck.
-            </div>
-            <p className="one">By KEVIN ROOSE</p>
-          </div>
-          <div className="article-image">
-            <img src="https://bit.ly/32e9oFd" width="342.22px" height="223.95px"/>
-          </div>
-          
-        </div>
-
-
-        <div className="container">
-          <div className="article-date">
-            <p>Jan. 30, 2018</p>
-          </div>
-          <div className="article-content">
-
-   
-            <h1>Taiwan Retaliates Against ChineseAirlines, Hampering Lunar New Year Travel </h1>
-            <div>
-   Taiwan, pushing back over encroachment on Taiwan Strait airspace, ay leave thousands without flights home for the holiday.
-            </div>
-            <p className="one">By CHRIS HORTON</p>
-          </div>
-          <div className="article-image">
-            <img src="https://bit.ly/35uuh0O" width="342.22px" height="223.95px"/>
-          </div>
-          
-        </div>
-
-
-
-        <div className="container">
-          <div className="article-date">
-            <p>Jan. 29, 2018</p>
-          </div>
-          <div className="article-content">
-
-         
-            <h1>New Jersey Embraces an Idea it Once Rejected: Make Utilities Pay to Emit Carbon</h1>
-            <div>
-              Democratic governors nationwide are taking steps to tax or price emissions within their own borders, even as Trump dismantles federal climate policy.
-            </div>
-            <p className="one">BRAD PLUMER</p>
-          </div>
-          <div className="article-image">
-            <img src="https://bit.ly/2MiP6EW" width="342.22px" height="223.95px"/>
-          </div>
-          
-        </div>
-
-
-
-
+        <Article date="Jan. 30, 2018" title="New Jersey Embraces an Idea it Once Rejected: Make Utilities Pay to Emit Carbon" paragraph="What's a 130-year-old company doing dabbling in cryptocurrency? Either revolutionizing digital rights management or trying to make a quick buck." image="https://bit.ly/32e9oFd" author="KEVIN ROOSE" />
+        <Article date="Jan. 29, 2018" title="Taiwan Retaliates Against Chinese Airlines, Hampering Lunar New Year Travel" paragraph="Taiwan, pushing back over encroachment on Taiwan Strait airspace, ay leave thousands without flights home for the holiday." image="https://bit.ly/35uuh0O" author="CHRIS HORTON" />
+        <Article date="Jan. 30, 2018" title="Kodak's Dubious Blockchain Gamble" paragraph="Democratic governors nationwide are taking steps to tax or price emissions within their own borders, even as Trump dismantles federal climate policy." image="https://bit.ly/2MiP6EW" author="BRAD PLUMER" />
       </div>
-  );
+  );
+}
+
+function Article(props) {
+  return(
+    <div className="container">
+        <div className="article-date">
+          <p>{props.date}</p>
+        </div>
+        <div className="article-content">
+          <p >THE SHIFT</p>
+          <h1>{props.title}</h1>
+
+          <div>
+            <p>{props.paragraph}</p>
+          </div>
+          <p className="one">By {props.author}</p>
+        </div>
+        <div className="article-image">
+          <img src={props.image} width="342.22px" height="223.95px"/>
+        </div>
+      </div>
+  )
+  
 }
 
 export default App;
