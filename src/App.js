5a268525db311a7d13c095aa7b40e333562b7513
Sara Gradinarska <saragradinarska@Saras-MacBook-Air-8.local>
2019-10-15 22:09:30 +0800

    app edits

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.svg'; 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App() {
+    return (
+      <div>
+
+
+        <div className="container">
+          <div className="article-date">
+            <p>Jan. 30, 2018</p>
+          </div>
+          <div className="article-content">
+
+            <p >THE SHIFT</p>
+            <h1>Kodak's Dubious Blockchain Gamble</h1>
+            <div>
+              What's a 130-year-old company doing dabbling in cryptocurrency? Either revolutionizing digital rights management or trying to make a quick buck.
+            </div>
+            <p className="one">By KEVIN ROOSE</p>
+          </div>
+          <div className="article-image">
+            <img src="https://bit.ly/32e9oFd" width="342.22px" height="223.95px"/>
+          </div>
+          
+        </div>
+
+
+        <div className="container">
+          <div className="article-date">
+            <p>Jan. 30, 2018</p>
+          </div>
+          <div className="article-content">
+
+   
+            <h1>Taiwan Retaliates Against ChineseAirlines, Hampering Lunar New Year Travel </h1>
+            <div>
+   Taiwan, pushing back over encroachment on Taiwan Strait airspace, ay leave thousands without flights home for the holiday.
+            </div>
+            <p className="one">By CHRIS HORTON</p>
+          </div>
+          <div className="article-image">
+            <img src="https://bit.ly/35uuh0O" width="342.22px" height="223.95px"/>
+          </div>
+          
+        </div>
+
+
+
+        <div className="container">
+          <div className="article-date">
+            <p>Jan. 29, 2018</p>
+          </div>
+          <div className="article-content">
+
+         
+            <h1>New Jersey Embraces an Idea it Once Rejected: Make Utilities Pay to Emit Carbon</h1>
+            <div>
+              Democratic governors nationwide are taking steps to tax or price emissions within their own borders, even as Trump dismantles federal climate policy.
+            </div>
+            <p className="one">BRAD PLUMER</p>
+          </div>
+          <div className="article-image">
+            <img src="https://bit.ly/2MiP6EW" width="342.22px" height="223.95px"/>
+          </div>
+          
+        </div>
+
+
+
+
+      </div>
+  );
 }
 
-export default App;
+export default App;
